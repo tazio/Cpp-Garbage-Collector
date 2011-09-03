16de51c3723a21d1c6237d9f46fa456821da6f62
Tazio Ceri <praise@praisenet.darktech.org>
2011-09-04 00:29:26 +0200

    Added license files

--- library/smart_pointers.hpp
+++ library/smart_pointers.hpp
@@ -1,3 +1,21 @@
+/**
+
+	Copyright (C) 2011 Tazio Ceri
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __SMART_POINTERS_HPP
 #define __SMART_POINTERS_HPP
 #include "utilities.hpp"
--- library/utilities.hpp
+++ library/utilities.hpp
@@ -1,9 +1,23 @@
+/**
+
+	Copyright (C) 2011 Tazio Ceri
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __UTILITIES_HPP
 #define __UTILITIES_HPP
-
-#if __GNUC__ < 4
-#warning "pippip"
-#endif
 
 #if __GNUC__ < 4 || ( __GNUC__ < 5 &&__GNUC_MINOR < 6 ) //nullptr is supported in gcc 4.6
 
