3c8b94bc9a9ac4c8cbbbde9b2b92784100acf9e7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2011-09-04 11:29:28 +0200

    first total definition

--- library/dummy_allocator.hpp
+++ library/dummy_allocator.hpp
@@ -16,6 +16,12 @@ public:
     typedef const _Tp& const_reference;
     typedef _Tp        value_type;
 
+    template<typename U>
+    struct rebind {
+        typedef dummy_allocator<U> other;
+    };
+
+
     dummy_allocator() throw(){};
     dummy_allocator(const dummy_allocator&) throw(){};
 
@@ -47,6 +53,36 @@ template <typename _Tp> dummy_allocator<_Tp>::pointer dummy_allocator<_Tp>::addr
 template <typename _Tp> dummy_allocator<_Tp>::const_pointer dummy_allocator<_Tp>::address(const_reference x) const
 {
     return(&x);
+}
+
+template <typename _Tp> dummy_allocator<_Tp>::size_type dummy_allocator<_Tp>::max_size() const throw()
+{
+    return size_t(-1) / sizeof(_Tp);
+}
+
+
+// memory managment
+template <typename _Tp> dummy_allocator<_Tp>::pointer dummy_allocator<_Tp>::allocate(size_type n, dummy_allocator<void>::const_pointer hint)
+{
+    if (n > this->max_size())
+        std::__throw_bad_alloc();
+
+    return( static_cast<pointer>(::operator new(n * sizeof(_Tp))) );
+}
+
+template <typename _Tp> void dummy_allocator<_Tp>::deallocate(pointer p, size_type n)
+{
+    ::operator delete(p);
+}
+
+template <typename _Tp> void dummy_allocator<_Tp>::construct(pointer p, const_reference val)
+{
+    ::new(static_cast<void *>(p)) _Tp(val);
+}
+
+template <typename _Tp> void dummy_allocator<_Tp>::destroy(pointer p)
+{
+    p->~Tp();
 }
 
 #endif // DUMMY_ALLOCATOR_HPP
